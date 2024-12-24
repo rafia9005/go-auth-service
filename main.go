5b99a8903900197e5561b6b1f2a74775349f775e
rafia9005 <rafia9005@gmail.com>
2024-12-24 19:20:07 +0700

    init(ResponWithJson): init on utils
    -add utils.go
    -add auth-handlers.go

--- main.go
+++ main.go
@@ -2,22 +2,35 @@ package main
 
 import (
 	"fmt"
+	"go-auth-service/pkg/utils"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-  // load the env
+	// load the env
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	r := mux.NewRouter()
 
-	fmt.Println("go auth service running on port: ")
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		payload := map[string]string{"message": "Hello, World!"}
+		utils.RespondJSON(w, http.StatusOK, payload)
+	})
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	port := os.Getenv("PORT")
+	if port == "" {
+		panic("PORT environment variable is not set")
+	}
+
+	fmt.Println("go auth service running on port:", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
