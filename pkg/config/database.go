da66a8b07ea9533b425d867e114e816ca634aa86
rafia9005 <rafia9005@gmail.com>
2024-12-24 19:23:50 +0700

    init(model): model like entity and request
    -add users.go

---
+++ pkg/config/database.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func DBConnect() error {
+	dsn := os.Getenv("APP_MYSQL")
+	fmt.Println(dsn)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Failed to connect database!")
+		return err
+	}
+
+	DB = db
+
+	// DB.AutoMigrate(entity.Users{})
+
+	fmt.Println("Database is connected!")
+	return nil
+}
